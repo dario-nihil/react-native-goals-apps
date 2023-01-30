42458155a7380c408f79ac53e3c363bae31676f6
dario-nihil <dario.nihil@gmail.com>
2023-01-30 19:14:24 +0100

    added state for inputted goals and added event listener to the inputText and button

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    console.log(enteredGoalText);
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="Add goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   appContainer: {
@@ -40,3 +55,5 @@ const styles = StyleSheet.create({
     flex: 5,
   },
 });
+
+export default App;
