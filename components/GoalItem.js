ecd0bcafa5ea4b9c0bd1b26b2951c0d9237b7b87
dario-nihil <dario.nihil@gmail.com>
2023-01-31 21:17:24 +0100

    added ripple style for android and opacity style for apple

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -4,11 +4,15 @@ const GoalItem = ({ dataItem, onDeleteGoal }) => {
   const { item } = dataItem;
 
   return (
-    <Pressable onPress={onDeleteGoal.bind(this, item.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#220548" }}
+        onPress={onDeleteGoal.bind(this, item.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{item.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 };
 
@@ -17,11 +21,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
+    padding: 8,
     color: "#ffffff",
   },
 });
